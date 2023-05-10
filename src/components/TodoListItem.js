5f99bf3a70365b936f5d922cc26dd99af8973a9a
hsnachos <hsnachos@gmail.com>
2023-05-10 16:41:19 +0900

    react-virtualized를 통한 성능개선

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,19 +7,24 @@ import React from 'react';
 import './TodoListItem.scss';
 import cn from 'classnames';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
 
   console.log(text);
 
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove">
-        <MdRemoveCircleOutline onClick={() => onRemove(id)} />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove">
+          <MdRemoveCircleOutline onClick={() => onRemove(id)} />
+        </div>
       </div>
     </div>
   );
